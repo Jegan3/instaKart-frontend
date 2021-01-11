8c4703b70ffc81b69d9a039c299c40a7c5da9567
Raki <rakiinfo7@gmail.com>
2021-01-12 00:00:23 +0530

    signup page chanegs

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -3,30 +3,13 @@ import { Row, Col, Image, Form, Grid } from 'react-bootstrap';
 import Footer from '../../components/Footer';
 
 const SignUp = () => (
-  // <div className="sign-container .col-lg 12">
   <Grid fluid>
-    <Row >
-      {/* left-div */}
-      <Col md={6} style={{ border: '1px solid black' }}>
-        <Row>
-          {/* <Col lg={12} className="title"> */}
-          {/* </Col> */}
-          <Image
-            className="signup-image"
-            src="https://st4.depositphotos.com/3474627/21320/v/1600/depositphotos_213206152-stock-illustration-freelancer-happy-young-men-working.jpg"
-            fluid
-          />
-          <Image className="signup-logo" src="images/logo.png" fluid />
-          {/* <Col lg={12} className="few-clicks">
-            A few more clicks to sign up to your Account
-          </Col>
-          <Col lg={12} className="few-cli">
-            Get 10% discount For Registration
-          </Col> */}
-        </Row>
+    <Row className="jumbotron signup-image">
+      <Col md={6} sm={6}>
+        <Image className="signup-logo" src="images/logo.png" fluid />
       </Col>
 
-      <Col md={6} className="right-div" fluid>
+      <Col md={6} sm={6} className="right-div" fluid>
         <Row>
           <Col lg={12} className="heading">
             Partner With us
@@ -40,20 +23,12 @@ const SignUp = () => (
         <Form className="form ">
           <Row>
             <Col md={6} sm={12}>
-              {/* <Form.Label>Plan *</Form.Label>
-              <Form.Control as="select">
-                <option>Please select</option>
-              </Form.Control> */}
               <label >Plan *</label>
               <select className="form-control">
                 <option>Default select</option>
               </select>
             </Col>
             <Col md={6} sm={12}>
-              {/* <Form.Label >Select Industry Type *</Form.Label>
-              <Form.Control as="select">
-                <option>Default select</option>
-              </Form.Control> */}
               <label >Select Industry Type *</label>
               <select className="form-control">
                 <option>Default select</option>
@@ -62,20 +37,12 @@ const SignUp = () => (
           </Row>
           <Row>
             <Col md={6} sm={12}>
-              {/* <Form.Label>Select Country *</Form.Label>
-              <Form.Control as="select">
-                <option>Default select</option>
-              </Form.Control> */}
               <label >Select Country *</label>
               <select className="form-control">
                 <option>Default select</option>
               </select>
             </Col>
             <Col md={6} sm={12} >
-              {/* <Form.Label>Select City *</Form.Label>
-              <Form.Control as="select">
-                <option>Default select</option>
-              </Form.Control> */}
               <label >Select City *</label>
               <select className="form-control">
                 <option>Default select</option>
@@ -84,28 +51,20 @@ const SignUp = () => (
           </Row>
           <Row>
             <Col md={6} sm={12}>
-              {/* <Form.Label>Owner Name *</Form.Label>
-              <Form.Control placeholder="Enter name" /> */}
               <label >Owner Name *</label>
               <input type="text" className="form-control" placeholder="Enter name"></input>
             </Col>
             <Col md={6} sm={12}>
-              {/* <Form.Label>Contact Number *</Form.Label>
-              <Form.Control placeholder="Last name" /> */}
               <label >Contact Number *</label>
               <input type="text" className="form-control" placeholder="Enter name"></input>
             </Col>
           </Row>
           <Row>
             <Col md={6} sm={12}>
-              {/* <Form.Label>Email *</Form.Label>
-              <Form.Control placeholder="Enter name" /> */}
               <label >Email *</label>
               <input type="email" className="form-control" placeholder="Enter name"></input>
             </Col>
             <Col md={6} sm={12}>
-              {/* <Form.Label>User Name *</Form.Label>
-              <Form.Control placeholder="Enter name" /> */}
               <label >User Name *</label>
               <input type="text" className="form-control" placeholder="Enter name"></input>
             </Col>
@@ -127,7 +86,6 @@ const SignUp = () => (
       <Footer />
     </Row>
   </Grid>
-  // </div>
 );
 
 export default SignUp;
