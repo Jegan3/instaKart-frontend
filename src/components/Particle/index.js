9f9f3839f115505ff14f7c2635da1feb3c4bd808
KeithReshma <gopinath.chandar@gmail.com>
2021-07-13 23:37:40 +0530

    temp fix

--- src/components/Particle/index.js
+++ src/components/Particle/index.js
@@ -1,64 +1,64 @@
-// /*eslint-disable*/
-// import React from 'react';
-// import Particles from 'react-particles-js';
+/*eslint-disable*/
+import React from 'react';
+import Particles from 'react-particles-js';
 
-// export default function Particle({ color, number, direction }) {
-//   return (
-//     <Particles
-//       params={{
-//         particles: {
-//           color: {
-//             value: `${color}`,
-//           },
-//           number: {
-//             value: `${number}`,
-//             density: {
-//               enable: false,
-//             },
-//           },
-//           line_linked: {
-//             enable: false,
-//           },
-//           size: {
-//             value: '3',
-//             random: true,
-//             anim: {
-//               speed: '3',
-//               size_min: '0.3',
-//             },
-//           },
-//           move: {
-//             random: true,
-//             speed: '1',
-//             direction: `${direction}`,
-//             out_mode: 'out',
-//           },
-//         },
-//         interactivity: {
-//           events: {
-//             // onhover: {
-//             //   enable: false,
-//             //   mode: 'bubble',
-//             // },
-//             onclick: {
-//               enable: true,
-//               mode: 'repulse',
-//             },
-//           },
-//           modes: {
-//             bubble: {
-//               distance: '250',
-//               duration: '2',
-//               size: '0',
-//               opacity: ' 0',
-//             },
-//             repulse: {
-//               distance: '100',
-//               duration: '4',
-//             },
-//           },
-//         },
-//       }}
-//     />
-//   )
-// }
+export function Particle({ color, number, direction }) {
+  return (
+    <Particles
+      params={{
+        particles: {
+          color: {
+            value: `${color}`,
+          },
+          number: {
+            value: `${number}`,
+            density: {
+              enable: false,
+            },
+          },
+          line_linked: {
+            enable: false,
+          },
+          size: {
+            value: '3',
+            random: true,
+            anim: {
+              speed: '3',
+              size_min: '0.3',
+            },
+          },
+          move: {
+            random: true,
+            speed: '1',
+            direction: `${direction}`,
+            out_mode: 'out',
+          },
+        },
+        interactivity: {
+          events: {
+            // onhover: {
+            //   enable: false,
+            //   mode: 'bubble',
+            // },
+            onclick: {
+              enable: true,
+              mode: 'repulse',
+            },
+          },
+          modes: {
+            bubble: {
+              distance: '250',
+              duration: '2',
+              size: '0',
+              opacity: ' 0',
+            },
+            repulse: {
+              distance: '100',
+              duration: '4',
+            },
+          },
+        },
+      }}
+    />
+  )
+}
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -4,9 +4,9 @@ import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import { CarouselFirst, CarouselSecond, CarouselThird, CarouselFourth, CarouselMobile, CarouselNewEStore, } from '../../components/Carousel';
 import { Banners, SecondaryBanners } from '../../components/Banners';
-import Particle from '../../components/Particle';
+import { Particle } from '../../components/Particle';
 
-const Home = () => {
+function Home () {
 
   useEffect(() => {
     // dispatch it once mounted
@@ -19,7 +19,7 @@ const Home = () => {
   return (
     <div className='body-container home'>
       <div className='particles-main'>
-        {/* <Particle color='#28a4d9' number='2100' direction='top' /> */}
+        <Particle color='#28a4d9' number='2100' direction='top' />
       </div>
       <div>
         <Header />
@@ -55,7 +55,7 @@ const Home = () => {
       </div>
       <div className='bg-home-pro'>
         <div className='particles-sub'>
-          {/* <Particle color='#f5f5f5' number='700' direction='left' /> */}
+          <Particle color='#f5f5f5' number='700' direction='left' />
         </div>
         <div className='arrival'>
           <h5>New Promotions</h5>
@@ -74,7 +74,7 @@ const Home = () => {
       </div>
       <div className='bg-home-pro'>
         <div className='particles-sub'>
-          {/* <Particle color='#f5f5f5' number='700' direction='right' /> */}
+          <Particle color='#f5f5f5' number='700' direction='right' />
         </div>
         <div className='arrival'>
           <h5>E-Stores Near You</h5>
