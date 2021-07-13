81901df2857dc375253db8d36669238bee70c1a7
KeithReshma <gopinath.chandar@gmail.com>
2021-07-13 22:47:35 +0530

    small particle tweaks

--- src/components/Particle/index.js
+++ src/components/Particle/index.js
@@ -2,63 +2,63 @@
 import React from 'react';
 import Particles from 'react-particles-js';
 
-const Particle = ({ color, number, direction }) => 
-
-<Particles
-    params={{
-	    "particles": {
-        "color": {
-          "value": `${color}`,
+export default function Particle({ color, number, direction }) {
+  return (
+    <Particles
+      params={{
+        particles: {
+          color: {
+            value: `${color}`,
+          },
+          number: {
+            value: `${number}`,
+            density: {
+              enable: false,
+            },
+          },
+          line_linked: {
+            enable: false,
+          },
+          size: {
+            value: '3',
+            random: true,
+            anim: {
+              speed: '3',
+              size_min: '0.3',
+            },
+          },
+          move: {
+            random: true,
+            speed: '1',
+            direction: `${direction}`,
+            out_mode: 'out',
+          },
         },
-	        "number": {
-	            "value": `${number}`,
-	            "density": {
-	                "enable": false,
-	            }
-	        },
-	        "size": {
-	            "value": 3,
-	            "random": true,
-	            "anim": {
-	                "speed": 3,
-	                "size_min": 0.3
-	            }
-	        },
-	        "line_linked": {
-	            "enable": false
-	        },
-	        "move": {
-	            "random": true,
-	            "speed": 1,
-	            "direction": `${direction}`,
-	            "out_mode": "out"
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            // "onhover": {
-	            //     "enable": true,
-	            //     "mode": "bubble"
-	            // },
-	            "onclick": {
-	                "enable": true,
-	                "mode": "repulse"
-	            }
-	        },
-	        "modes": {
-	            "bubble": {
-	                "distance": 250,
-	                "duration": 2,
-	                "size": 0,
-	                "opacity": 0
-	            },
-	            "repulse": {
-	                "distance": 100,
-	                "duration": 4
-	            }
-	        }
-	    }
-	}} />
-
-export default Particle;
-
+        interactivity: {
+          events: {
+            // onhover: {
+            //   enable: false,
+            //   mode: 'bubble',
+            // },
+            onclick: {
+              enable: true,
+              mode: 'repulse',
+            },
+          },
+          modes: {
+            bubble: {
+              distance: '250',
+              duration: '2',
+              size: '0',
+              opacity: ' 0',
+            },
+            repulse: {
+              distance: '100',
+              duration: '4',
+            },
+          },
+        },
+      }}
+    />
+  )
+}
