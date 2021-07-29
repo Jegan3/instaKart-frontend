018e586c048c34c8f98813888f733074b7a77f0d
Rakii <Rakii@Rakiis-MacBook-Air.local>
2021-07-29 20:13:40 +0530

    myprofile changes

--- src/containers/MyProfile/index.js
+++ src/containers/MyProfile/index.js
@@ -1,70 +1,104 @@
 /*eslint-disable*/
-import React, { useState } from 'react';
-import ImgCrop from 'antd-img-crop';
-import { Upload } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Headerbar from '../../components/Headerbar';
 import Sidebar from '../../components/Sidebar';
+import { Row, Col } from 'react-bootstrap';
+import ImgCrop from 'antd-img-crop';
+import { Upload } from 'antd';
+
 
 const MyProfile = () => {
   const [companyLogo, setCompanyLogo] = useState('');
 
+  const dispatch = useDispatch();
+  const thriftVendorInfo = useSelector((state) => state.thriftVendorInfoState.thriftVendorInfo);
 
-  // const onAvatarImage = ({ fileList: newFileList }) => {
-  //   let file = newFileList[0].originFileObj;
-  //   const reader = new FileReader();
-  //   reader.onload = () => {
-  //     setCompanyLogo(reader.result);
-  //   };
-  //   reader.readAsDataURL(file);
-  // }
+  useEffect(() => {
+    dispatch({ type: 'THRIFT_VENDOR_INFO_REQUEST' });
+  }, [])
+
+  let fileList = [];
+
+  const onAvatarImage = ({ fileList: newFileList }) => {
+    let file = newFileList[0].originFileObj;
+    const reader = new FileReader();
+    reader.onload = () => {
+      setCompanyLogo(reader.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  const fakeRequest = ({ onSuccess }) => {
+    setTimeout(() => {
+      onSuccess('OK')
+    }, 1000)
+  }
 
   return (
     <div className="wrapper">
-    
-    <Sidebar />
-    <div className="rightside-panel">
-      <Headerbar headerName="My-Profile"/>
-      <div className="m-profile">
-          <div className='my-profile'>
-            <div class="profile-card">
-              <header>
-              {/* <div className='load-info'>
-                        <div>
-                          <div className="photo">
-                          <img src={companyLogo} alt='' />
-              {companyLogo ? <img src={companyLogo} alt='' /> : 
-              <img src={thriftVendorInfo && thriftVendorInfo.vendorInfo.logo ? thriftVendorInfo.vendorInfo.logo : 
-              "images/Your-logo-here..png"} />} 
-                          </div>
-                          <div className="image-upload">
-                            <ImgCrop rotate>
-                              <Upload
-                                fileList={fileList}
-                                customRequest={fakeRequest}
-                                onChange={onAvatarImage}
-                                type="file"
-                                accept="image/*"
-                                showUploadList={false}
-                              >
-                                <label for="file-input">
-                                  <i className="fa fa-camera" />
-                                </label>
-                              </Upload>
-                            </ImgCrop>
-                          </div>
+      <Sidebar />
+      <div className="rightside-panel">
+        <Headerbar headerName="My-Profile" />
+        <div className='my-profile'>
+          <div className="profilelist">
+            <Row>
+              <Col md={6}>
+                <div className="avatardetails cardetails">
+                  <Row>
+                    <Col md={12} className='avtar-info' >
+                      <div className="profilephoto">
+                        <img src="images/Your-logo-here..png" />
+                      </div>
+                    </Col>
+                    <Col md={12} >
+                      <div clasName="personal-details" >
+                        <div className="label-myprofile">
+                          <span>Name :  Jennifer  </span>
                         </div>
-                      </div> */}
-                <h1>David Jones</h1>
-                <h2>1212345678</h2>
-              </header>
-              {/* <div class="profile-bio">
-                <p>Even when everything is perfect, you can always make it better.
-                   Break barriers in your head, create something crazy and don't 
-                   forget Code is Poetry...</p>
-              </div> */}
-            </div>
+                        <div className="label-myprofile">
+                          <span>Name :  Jennifer  </span>
+                        </div>
+                        <div className="label-myprofile">
+                          <span>Name :  Jennifer  </span>
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+              </Col>
+              <Col md={6}>
+                <Row>
+                  <Col md={12}>
+                    <div className="cardetails">
+                      <div className="label-myprofile">
+                        <span>Name :  Jennifer  </span>
+                      </div>
+                      <div className="label-myprofile">
+                        <span>Name :  Jennifer  </span>
+                      </div>
+                      <div className="label-myprofile">
+                        <span>Name :  Jennifer  </span>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col md={12}>
+                    <div className="cardetails">
+                      <div className="label-myprofile">
+                        <span>Name :  Jennifer  </span>
+                      </div>
+                      <div className="label-myprofile">
+                        <span>Name :  Jennifer  </span>
+                      </div>
+                      <div className="label-myprofile">
+                        <span>Name :  Jennifer  </span>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
           </div>
-          
         </div>
       </div>
     </div>
