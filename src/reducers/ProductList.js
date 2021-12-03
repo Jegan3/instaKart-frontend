817a8e28d8f1d53dd1e21d0ffbd1c8f4bb81dac1
Rakii <rakii@Rakiis-MacBook-Air.local>
2021-12-03 19:20:00 +0530

    product list delete

--- src/reducers/ProductList.js
+++ src/reducers/ProductList.js
@@ -1,6 +1,6 @@
 const initialState = {};
 
-export default (state = initialState, action) => {
+export const productListState = (state = initialState, action) => {
   switch (action.type) {
     case 'PRODUCT_LIST_REQUEST':
       return {
@@ -15,6 +15,31 @@ export default (state = initialState, action) => {
         productList: action.productList,
       };
     case 'PRODUCT_LIST_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
+export const productDeleteState = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PRODUCT_DELETE_REQUEST':
+      return {
+        ...state,
+        isLoading: true,
+        error: false,
+      };
+    case 'PRODUCT_DELETE_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        productDelete: action.productDelete,
+      };
+    case 'PRODUCT_DELETE_FAILURE':
       return {
         ...state,
         isLoading: false,
