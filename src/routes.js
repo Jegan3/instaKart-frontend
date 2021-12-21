2eea7ba3d0fea0dfb1dc06590ef09e41e907eb39
Manikandan <nmanikandan2211@gmail.com>
2021-12-21 18:23:17 +0530

    store info validation changes

--- src/routes.js
+++ src/routes.js
@@ -11,15 +11,13 @@ import AboutUs from './pages/User/AboutUs';
 import VendorInfo from './pages/Vendor/VendorInfo';
 import ThriftStore from './pages/User/ThriftStore';
 import ComingSoon from './pages/User/ComingSoon';
-import ProductList from './pages/Vendor/ProductList';
-import AddProduct from './pages/Vendor/AddProduct';
 import StoreInfo from './pages/Vendor/StoreInfo';
 import UpdatePassword from './pages/User/UpdatePassword';
 import Profile from './pages/Vendor/Profile';
 import ProductInfo from './pages/User/ProductInfo';
 import ThriftStoreInfo from './pages/User/ThriftStoreInfo';
 import Cart from './pages/User/Cart';
-import Industry from './pages/Vendor/Industry';
+import Store from './pages/Vendor/Store';
 import ListYourAds from './pages/User/ListYourAds';
 import PaymentStatus from './pages/User/PaymentStatus';
 import User from './pages/Admin/User';
@@ -33,11 +31,9 @@ const AuthenticatedRoutes = () => (
       <Sidebar />
       <Switch>
         <Switch>
-          <Route path="/productlist" component={ProductList} />
-          <Route path="/addproduct" component={AddProduct} />
-          <Route path="/storeInfo" component={StoreInfo} />
+          <Route path="/storeinfo" component={StoreInfo} />
           <Route path="/profile" component={Profile} />
-          <Route path="/storedetails" component={Industry} />
+          <Route path="/storedetails" component={Store} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/vendor" component={Vendor} />
           <Route path="/user" component={User} />
